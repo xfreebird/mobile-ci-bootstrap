2f8bd636ab9da85eae6f20d5c7fea1bb5b3cc693
Nicolae Ghimbovschi <xfreebird@gmail.com>
2018-10-27 18:54:47 +0300

    Update mobile-ci-bootstrap.sh

--- mobile-ci-bootstrap.sh
+++ mobile-ci-bootstrap.sh
@@ -26,6 +26,7 @@ function updateXcodeBuildTools() {
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
   softwareupdate -i "$PROD" --verbose
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
   softwareupdate -i "$PROD" --verbose
   rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
